a401d769be4b62e17dbedbe93bbbae9907d4cb21
antoniolm23 <antoniolm23@gmail.com>
2014-02-06 16:10:14 +0100

    added restrictions

--- header.h
+++ header.h
@@ -2,5 +2,7 @@
  * 
  * @Authors: Vecere Giacomo Razzano Alessia Piras Francesco La Marra Antonio
  * @Topic: UDP Paris-Traceroute to identify Third Party Addresses
- * 
+ * USE OF IPv4
+ * ONLY SUPERUSER CAN CREATE RAW DATAGRAMS HENCE TO RUN THE PROGRAM
+ * YOU NEED TO BE SUPERUSER
  */
