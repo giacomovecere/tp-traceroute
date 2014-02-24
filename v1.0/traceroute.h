094a6d06bbc5f11e70ac0f2b86a933082fc8fa76
Giacomo <imperatorejack@hotmail.it>
2014-02-24 12:28:17 +0100

    v1.0 added - traceroute changed

---
+++ v1.0/traceroute.h
@@ -0,0 +1,38 @@
+/*
+ * 
+ * @Authors: Vecere Giacomo Razzano Alessia Piras Francesco La Marra Antonio
+ * @Topic: UDP Paris-Traceroute to identify Third Party Addresses
+ * USE OF IPv4
+ * ONLY SUPERUSER CAN CREATE RAW DATAGRAMS HENCE TO RUN THE PROGRAM
+ * YOU NEED TO BE SUPERUSER
+ * 
+ * Traceroute Header File
+ * 
+ */
+ 
+#include "udp.h"
+#include "icmp.h"
+
+class traceroute {
+    list<addr> array_ip_list[MAX_TTL_DEF];
+    uint16_t src_port; 
+    int last_position;
+    
+    public:
+    
+    traceroute(uint16_t);
+    ~traceroute();
+    
+    /* computes the traceroute to the destination: 'ip_address'
+    * 'max_ttl' indicates the max value for the TTL
+    * 'dest_port_ini' is the first value of the destination port. It is going to be incremented 
+    * for finding one from which the router along the path will reply */
+    bool trace(char*, int, uint16_t);
+    
+    /* Returns the pointer of the array that contains the lists of the 'addr' elements */
+    list<addr>* getArrayList();
+    
+    /* Prints the elements of the list stored */
+    void print();
+	
+};
