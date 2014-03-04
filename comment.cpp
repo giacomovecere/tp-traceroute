603a1894d7693198818cb1d2b2cf35a35cc63275
Giacomo <imperatorejack@hotmail.it>
2014-03-04 15:11:41 +0100

    comment.cpp added

---
+++ comment.cpp
@@ -0,0 +1,20 @@
+/* instestazione */
+
+Commenti a funzione:
+
+/* Purpose: 
+ * Parameters: 
+	in 
+	out
+ */
+void function(){}
+
+a = 5;
+for(int i=1; i<5; i++);
+
+//
+if(c < 5) {
+}
+
+max size 80 characters
+
